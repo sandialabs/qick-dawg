815b4232d3749bd7995f58f9d663b28969689638
egriend <egriend@sandia.gov>
2024-04-23 21:21:49 -0400

    logo only issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = "_static/qickdawg_forgithub.png"
 html_theme_options = {
-    'logo-only': True,
+    'logo_only': True,
     'display_version': False,
 }
 
