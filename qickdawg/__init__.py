efe2b0069b64a1edfb3cd989c3ddffb5b6a658fa
i-am-mounce <amounce@sandia.gov>
2024-10-09 09:23:58 -0600

    fix(init): updated init message

--- qickdawg/__init__.py
+++ qickdawg/__init__.py
@@ -2,4 +2,4 @@ from qickdawg.util import *
 from qickdawg.nvpulsing import *
 from qickdawg.fitfunctions import *
 
-# Now using QICK 0.2.160 as of 7/03/2023
+# Now using QICK 0.2.289 as of 10/9/2024
